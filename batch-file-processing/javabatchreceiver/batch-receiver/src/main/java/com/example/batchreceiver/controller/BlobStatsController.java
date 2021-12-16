e841a478a91a31c399e9bca0979de5e55f952e97
msmarti <ms-andrem@hotmail.com>
2021-12-16 21:24:24 +0000

    Added BlobStats Controller

---
+++ batch-file-processing/javabatchreceiver/batch-receiver/src/main/java/com/example/batchreceiver/controller/BlobStatsController.java
@@ -0,0 +1,62 @@
+package com.example.batchreceiver.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.azure.messaging.eventgrid.systemevents.StorageBlobCreatedEventData;
+import com.azure.storage.queue.*;
+import com.azure.storage.queue.models.*;
+
+import reactor.core.publisher.Mono;
+
+class Stats
+{
+  int tasks;
+} 
+
+@RestController
+public class BlobStatsController {
+
+  static final String BINDING_NAME = "dapr-output-queue";
+  static final String connectStr = "DefaultEndpointsProtocol=https;AccountName=dapr1batch;AccountKey=ZSDksBXW3ObRXqDKdmH/JqRno9Gw7Y9IRPgTmKvCyBWozvbD/mNQxE1jKcM96ya81oAB+G/43BGbdlSZ4QPdbg==;EndpointSuffix=core.windows.net";
+
+  @GetMapping("/api/blobstats")
+  public ResponseEntity<Stats> getBlobStats() {
+
+    String filesize = "";
+    Stats stats = new Stats();
+    
+    try
+    {
+        // Instantiate a QueueClient which will be
+        // used to create and manipulate the queue
+        QueueClient queueClient = new QueueClientBuilder()
+                                    .connectionString(connectStr)
+                                    .queueName(BINDING_NAME)
+                                    .buildClient();
+
+       // Get the first queue message
+       QueueMessageItem message = queueClient.receiveMessage();
+       
+
+       if (null != message)
+       {
+           System.out.println("Message retrieved: " + message);
+           
+           StorageBlobCreatedEventData blobCreatedData = message.getBody().toObject(StorageBlobCreatedEventData.class);
+           filesize = blobCreatedData.getContentLength().toString();
+           stats.tasks = Integer.valueOf(filesize);          
+       }       
+    }
+    catch (QueueStorageException e)
+    {
+        // Output the exception message and stack trace
+        System.out.println(e.getMessage());
+        e.printStackTrace();
+    }
+    return new ResponseEntity<>(stats, HttpStatus.OK); 
+  }
+}
